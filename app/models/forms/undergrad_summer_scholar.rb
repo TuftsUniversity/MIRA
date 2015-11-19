e2530d2414ab007abc19623359c3a1b422611471
Mike Korcynski <mkorcy@gmail.com>
2015-11-19 15:29:12 -0500

    adds undergrad summer scholars self-deposit

---
+++ app/models/forms/undergrad_summer_scholar.rb
@@ -0,0 +1,8 @@
+class UndergradSummerScholar < GenericTischDeposit
+
+  def copy_attributes
+    super
+    @tufts_pdf.description += ["Submitted in partial fulfillment of the grant requirement of the Tufts Summer Scholars Program.  Abstract: #{description}"]
+  end
+
+end
